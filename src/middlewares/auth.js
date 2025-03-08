44e824e52b8b691f9ed802299a75f40e8a84a5b8
rohitranjanab1718 <rohitranjanab1718@gmail.com>
2025-03-08 16:36:41 +0530

    code commit

---
+++ src/middlewares/auth.js
@@ -0,0 +1,27 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+
+const userAuth = async(req,res,next)=>{
+    try{
+        const {token} = req.cookies;
+
+        if (!token){
+            return res.status(401).send("Please Login!");
+        }
+
+        const decodedObj = jwt.verify(token, process.env.JWT_SECRET);
+        const {_id} = decodedObj;
+        const user = await User.findById(_id);
+        if (!user){
+            throw new Error("User Not Found");
+        }
+        req.user = user;
+        next();
+    }catch(error){
+        res.status(400).send("ERROR: " + err.message);
+    }
+}
+
+module.exports = {
+   userAuth 
+}
